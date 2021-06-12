73c8ff8726f7ed4e0b7dcebdae02eb467de9d494
Erin Buckner <85509494+erbuckner@users.noreply.github.com>
2021-06-12 20:39:02 +0000

    Satisfied conditions of Assignment Pt. 1

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,42 +3,44 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "";
+
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
+let questions = "";
+let correctAnswers = null;
+let candidateAnswers = null;
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+candidateName = (input.question("What is your name? "));
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+candidateAnswer = input.question(question);
 }
 
 function gradeQuiz(candidateAnswers) {
-
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-
-  let grade;
-  
-
+if (candidateAnswer == "Sally Ride") {
+  let grade = "Correct";
+  console.log(grade);
   return grade;
+  }
+ else {
+  console.log("Incorrect");
+  }
 }
+
+
 
 function runProgram() {
   askForName();
-  // TODO 1.1c: Ask for candidate's name //
-  
+  console.log("Hello, " + candidateName + "!")
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
